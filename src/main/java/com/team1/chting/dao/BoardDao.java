930cced5aecaa7972f1076a9a2e8843fe5feff7a
123 <123>
2021-06-07 20:31:41 +0900

    group recommend 6/7

--- src/main/java/com/team1/chting/dao/BoardDao.java
+++ src/main/java/com/team1/chting/dao/BoardDao.java
@@ -1,14 +1,21 @@
 package com.team1.chting.dao;
 
+import com.team1.chting.dto.AreaDto;
 import com.team1.chting.dto.GroupDto;
 
 import java.util.List;
 
 public interface BoardDao {
 
+    //지역정보
+    public List<AreaDto> getAreaList();
+
     //NEW 추천 모임
     public List<GroupDto> getNewGroupList();
 
     //BEST 추천 모임
     public List<GroupDto> getBestGroupList();
+
+    //모임검색
+    public List<GroupDto> getGroupListBySearch(String category, String search);
 }
