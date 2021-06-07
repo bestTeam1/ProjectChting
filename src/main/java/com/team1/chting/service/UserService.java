7ec8046ca7d9ea8bb439bb7b20ecf1243bc183e1
joohyun <joohyun93326@gmail.com>
2021-06-07 20:26:30 +0900

    0607 commit

--- src/main/java/com/team1/chting/service/UserService.java
+++ src/main/java/com/team1/chting/service/UserService.java
@@ -3,6 +3,8 @@ package com.team1.chting.service;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.team1.chting.dao.BoardDao;
+import com.team1.chting.dto.GroupDto;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,4 +15,35 @@ import com.team1.chting.dto.UserDto;
 @Service
 public class UserService {
 
+    @Autowired
+    private SqlSession sqlsession;
+
+    /*
+    마이페이지 회원 정보 불러오기
+    작성자 : 박주현
+    작성일 : 2021-06-07
+    */
+    public List<UserDto> userInfoBasic(){
+
+        UserDao userDao = sqlsession.getMapper(UserDao.class);
+        List<UserDto> UserInfoBasicList = userDao.userInfoBasic();
+
+        return UserInfoBasicList;
+    }
+
+    public List<UserDto> userInterest() {
+
+        UserDao userDao = sqlsession.getMapper(UserDao.class);
+        List<UserDto> UserInterestList = userDao.userInterest();
+
+        return UserInterestList;
+    }
+
+    public List<UserDto> userJoinGroup() {
+
+        UserDao userDao = sqlsession.getMapper(UserDao.class);
+        List<UserDto> UserJoinGroupList = userDao.userJoinGroup();
+
+        return UserJoinGroupList;
+    }
 }
