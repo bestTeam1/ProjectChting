6e37a1f8dce3b6c53a78a09dc1d7f7787c298f36
K-MyeongHwan <78404952+K-MyeongHwan@users.noreply.github.com>
2021-06-07 22:28:41 +0900

    get Recently Notice

--- src/main/java/com/team1/chting/service/NoticeService.java
+++ src/main/java/com/team1/chting/service/NoticeService.java
@@ -1,8 +1,24 @@
 package com.team1.chting.service;
 
+import com.team1.chting.dao.NoticeDao;
+import com.team1.chting.dto.NoticeDto;
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class NoticeService {
+    @Autowired
+    private SqlSession sqlSession;
 
+    public List<NoticeDto> recentlyNotice(){
+        List<NoticeDto> list = new ArrayList<NoticeDto>();
+        NoticeDao dao = sqlSession.getMapper(NoticeDao.class);
+        list = dao.recentlyNotice();
+
+        return list;
+    }
 }
