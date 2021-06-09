691c2468c9c97979231436da988349c60cc73f8f
123 <123>
2021-06-09 09:49:24 +0900

    6/8 모임찾기 commit

--- src/main/java/com/team1/chting/controller/BoardRestController.java
+++ src/main/java/com/team1/chting/controller/BoardRestController.java
@@ -1,16 +1,21 @@
 package com.team1.chting.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.team1.chting.dto.AreaDto;
 import com.team1.chting.dto.GroupDto;
 import com.team1.chting.service.BoardService;
+import org.apache.commons.compress.utils.Lists;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.util.List;
 
@@ -23,28 +28,26 @@ public class BoardRestController {
     @Autowired
     private BoardService boardService;
 
+    @RequestMapping(value="searchButton.do", method= RequestMethod.GET, produces = "application/json")
+    public ResponseEntity<String> searchGroup(@RequestParam("category") String category, @RequestParam("search") String search){
 
-    //비동기 Searching
-    @RequestMapping(value="searchButton.do",produces = "application/text; charset=utf8")
-    public String groupSearch(@RequestParam("category") String category, @RequestParam("search") String search) {
-        System.out.println("searchButton.do 들어옴");
-        System.out.println(category);
-        System.out.println(search);
-    /*    if(category.equals("전체")) {
-            System.out.println("전체네?");
-            category = "";
-            System.out.println("카테고리 : " + category);
-        }*/
+        List<GroupDto> list = null;
 
-        List<GroupDto> groupList = boardService.getGroupListBySearch(category, search);
+        ObjectMapper objmap = new ObjectMapper();
 
-        System.out.println("boardService 통과");
-        System.out.println("띠용?" + groupList);
+        String result = "";
 
-        String msg = "";
-
-        return msg;
+        try {
+            list = boardService.getGroupListBySearch(category, search);
+            for(GroupDto dto : list) {
+                //System.out.println(dto.toString());
+            }
+            System.out.println("RestController실행됨");
+            result = objmap.writeValueAsString(list);
+            return new ResponseEntity<String>(result, HttpStatus.OK);
+        } catch(Exception e){
+            //System.out.println(e.getMessage());
+            return new ResponseEntity<String>(result, HttpStatus.BAD_GATEWAY);
+        }
     }
-
-
 }
