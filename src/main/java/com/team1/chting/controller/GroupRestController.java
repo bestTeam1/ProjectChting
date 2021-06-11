7928e4ce07e7bc97b2517fa1f2e5f19e9909c8af
123 <123>
2021-06-11 17:52:50 +0900

    6/11 멤버관리 기능구현중

---
+++ src/main/java/com/team1/chting/controller/GroupRestController.java
@@ -0,0 +1,49 @@
+package com.team1.chting.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.team1.chting.dto.GroupDto;
+import com.team1.chting.dto.UserDto;
+import com.team1.chting.service.BoardService;
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class GroupRestController {
+
+    @Autowired
+    private SqlSession sqlSession;
+
+    @Autowired
+    private BoardService boardService;
+
+    @RequestMapping(value="banishMember.do", method= RequestMethod.GET, produces = "application/json")
+    public ResponseEntity<String> banishMember (@RequestParam("banishList[]") List<String> banishList, @RequestParam("groupNo") String groupNo){
+
+        System.out.println(banishList);
+
+        //boardService.deleteRequest(requestList, groupNo);
+        List<UserDto> list = null;
+
+        ObjectMapper objmap = new ObjectMapper();
+
+        String result = "";
+
+        try {
+            list = boardService.getGroupJoinRequest(groupNo);
+            result = objmap.writeValueAsString(list);
+            return new ResponseEntity<String>(result, HttpStatus.OK);
+        } catch (Exception e) {
+            return  new ResponseEntity<String>(result, HttpStatus.BAD_GATEWAY);
+        }
+
+    }
+
+}
