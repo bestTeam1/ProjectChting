76feed60ee7e493ffc94020dfc69be0572191564
K-MyeongHwan <78404952+K-MyeongHwan@users.noreply.github.com>
2021-06-13 14:34:39 +0900

    0613_sidebar_groupMain

--- src/main/java/com/team1/chting/controller/GroupController.java
+++ src/main/java/com/team1/chting/controller/GroupController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
@@ -22,34 +23,36 @@ public class GroupController {
 
     // 메인
     @RequestMapping(value = "board_main.do", method = RequestMethod.GET)
-    public String groupMain(Model model){
-
+    public String groupMain(@RequestParam("group_no") String group_no, Model model){
+        GroupDto dto = groupservice.groupByGroup_no(group_no);
+        model.addAttribute("group",dto);
         return "board/board_main";
     }
 
     // 게시물 리스트
     @RequestMapping(value = "board_list.do", method = RequestMethod.GET)
-    public String groupList(Model model) {
+    public String groupList(@RequestParam("group_no") String group_no, Model model) {
 
         return "board/board_list";
     }
 
     //글쓰기
     @RequestMapping(value = "board_write.do", method = RequestMethod.GET)
-    public String groupWrite(Model model){
+    public String groupWrite(@RequestParam("group_no") String group_no, Model model){
+
         return "board/board_write";
     }
 
     // 일정
     @RequestMapping(value = "board_diary.do", method = RequestMethod.GET)
-    public String groupDiary(Model model){
+    public String groupDiary(@RequestParam("group_no") String group_no, Model model){
 
         return "board/board_diary";
 }
 
     // 채팅
     @RequestMapping(value = "board_chatting.do", method = RequestMethod.GET)
-    public String groupChatting(Model model){
+    public String groupChatting(@RequestParam("group_no") String group_no,Model model){
 
         return "board/board_chatting";
     }
--- src/main/java/com/team1/chting/dao/BoardDao.java
+++ src/main/java/com/team1/chting/dao/BoardDao.java
@@ -2,6 +2,7 @@ package com.team1.chting.dao;
 
 import com.team1.chting.dto.AreaDto;
 import com.team1.chting.dto.GroupDto;
+import com.team1.chting.dto.PostDto;
 
 import java.util.List;
 
@@ -18,4 +19,7 @@ public interface BoardDao {
 
     //모임검색
     public List<GroupDto> getGroupListBySearch(String category, String search);
+
+    //게시판페이지 -> 그룹넘버를 이용한 검색
+    public List<PostDto> postByGroup_no(String Group_no);
 }
