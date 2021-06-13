0e2d5e08194d93f17b4c1d2a9e17d00d5e44ee9c
zagg2732 <zagg2732@gmail.com>
2021-06-14 00:35:30 +0900

    Controller정리, 모임관리 link sidebar 수정

--- src/main/java/com/team1/chting/controller/BoardRestController.java
+++ src/main/java/com/team1/chting/controller/BoardRestController.java
@@ -1,24 +1,17 @@
 package com.team1.chting.controller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.team1.chting.dto.AreaDto;
 import com.team1.chting.dto.GroupDto;
-import com.team1.chting.dto.UserDto;
 import com.team1.chting.service.BoardService;
-import org.apache.commons.compress.utils.Lists;
-import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import java.nio.file.attribute.GroupPrincipal;
 import java.util.List;
 
 @RestController
@@ -30,6 +23,7 @@ public class BoardRestController {
     @Autowired
     private BoardService boardService;
 
+    //모임 검색
     @RequestMapping(value="searchButton.do", method= RequestMethod.GET, produces = "application/json")
     public ResponseEntity<String> searchGroup(@RequestParam("category") String category, @RequestParam("search") String search){
 
@@ -52,43 +46,4 @@ public class BoardRestController {
             return new ResponseEntity<String>(result, HttpStatus.BAD_GATEWAY);
         }
     }
-
-    @RequestMapping(value="joinAccept.do", method= RequestMethod.GET, produces = "application/json")
-    public ResponseEntity<String> joinAccept(@RequestParam("requestList[]") List<String> requestList, @RequestParam("groupNo") String groupNo){
-        boardService.acceptMembers(requestList, groupNo);
-        List<UserDto> list = null;
-
-        ObjectMapper objmap = new ObjectMapper();
-
-        String result = "";
-
-        try {
-            list = boardService.getGroupJoinRequest(groupNo);
-            result = objmap.writeValueAsString(list);
-            return new ResponseEntity<String>(result, HttpStatus.OK);
-        } catch (Exception e) {
-            return  new ResponseEntity<String>(result, HttpStatus.BAD_GATEWAY);
-        }
-
-    }
-
-    @RequestMapping(value="joinDeny.do", method= RequestMethod.GET, produces = "application/json")
-    public ResponseEntity<String> joinDeny(@RequestParam("requestList[]") List<String> requestList, @RequestParam("groupNo") String groupNo){
-        boardService.deleteRequest(requestList, groupNo);
-        List<UserDto> list = null;
-
-        ObjectMapper objmap = new ObjectMapper();
-
-        String result = "";
-
-        try {
-            list = boardService.getGroupJoinRequest(groupNo);
-            result = objmap.writeValueAsString(list);
-            return new ResponseEntity<String>(result, HttpStatus.OK);
-        } catch (Exception e) {
-            return  new ResponseEntity<String>(result, HttpStatus.BAD_GATEWAY);
-        }
-
-    }
-
 }
