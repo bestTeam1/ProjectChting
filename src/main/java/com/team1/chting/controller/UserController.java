9b745c56c7d3e734f019ac0e5840feb34fc4393f
joohyun <joohyun93326@gmail.com>
2021-06-15 05:43:12 +0900

    관심사 선택 가능하게 수정

--- src/main/java/com/team1/chting/controller/UserController.java
+++ src/main/java/com/team1/chting/controller/UserController.java
@@ -1,9 +1,6 @@
 package com.team1.chting.controller;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -13,6 +10,7 @@ import com.team1.chting.service.BoardService;
 import net.sf.json.JSONArray;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.parameters.P;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -20,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.team1.chting.service.UserService;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class UserController {
@@ -67,18 +66,33 @@ public class UserController {
     작성일 : 2021-06-11
     */
     @RequestMapping(value = "categoryChoice.do", method = RequestMethod.GET)
-    public String categoryChoice(Model model) {
+    public String categoryChoice(Model model,
+                                 @RequestParam(value="catelist", defaultValue = "")
+                                         List<String> catelist) {
 
+        //System.out.println("catelist : " +catelist);
         List<InterestCategoryDto> list = new ArrayList<>();
-        InterestCategoryDto interestCategory = new InterestCategoryDto();
 
-        interestCategory.setM_catecode("");
-        list.add(interestCategory);
+        if (catelist.size() == 0) {
+            InterestCategoryDto interestCategory = new InterestCategoryDto();
+            interestCategory.setParent_catecode("");
+            list.add(interestCategory);
+        }else {
+            for(int i = 0; i < catelist.size(); i++) {
+                InterestCategoryDto interestCategory = new InterestCategoryDto();
+                //System.out.println(catelist.get(i));
+                interestCategory.setParent_catecode(catelist.get(i));
+
+                //System.out.println("interestCategory:" +interestCategory);
+                list.add(interestCategory);
+            }
+        }
+
+        System.out.println("list :" +list);
 
         model.addAttribute("interestCategory", userService.getInterestCategory(list));
 
         return "user/interestCategory";
     }
-
 
 }
