ca8acafb54499bd65c27ec61a645726f05cc96f4
zagg2732 <zagg2732@gmail.com>
2021-06-15 20:56:59 +0900

    관리자 게시판 공지사항 페이징

--- src/main/java/com/team1/chting/controller/AdminController.java
+++ src/main/java/com/team1/chting/controller/AdminController.java
@@ -1,17 +1,14 @@
 package com.team1.chting.controller;
 
-import com.team1.chting.dto.GroupDto;
-import com.team1.chting.dto.UserDto;
+import com.team1.chting.dto.NoticeDto;
 import com.team1.chting.service.AdminService;
 import com.team1.chting.utils.AdminCriteria;
-import com.team1.chting.utils.Criteria;
 import com.team1.chting.utils.PageMaker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -29,11 +26,31 @@ public class AdminController {
         return "admin/index";
     }
 
+    //공지사항 목록보기(PageMaker객체 사용)
     @RequestMapping(value = "adminNotice.do", method = RequestMethod.GET)
-    public String adminNotice(Model model) {
+    public String listPageGET(AdminCriteria cri, Model model) throws Exception {
+        model.addAttribute("boardList", adminService.listCri(cri));
 
+        PageMaker pm = new PageMaker();
+        pm.setCri(cri);
+        pm.setTotalCount(adminService.pageCount()); //DB의 전체ROW수 입력
+
+        // 뷰페이지로 전달
+        model.addAttribute("pm", pm);
 
         return "admin/notice";
+    }
+
+    //공지사항 상세보기
+    @RequestMapping(value = "adminNoticeDetail.do", method = RequestMethod.GET)
+    public String adminNoticeDetail(String noticeNo, Model model) {
+
+        NoticeDto noticeDto = adminService.getNoticeDetail(noticeNo);
+        System.out.println("noticeNo : " + noticeNo);
+        model.addAttribute("detail", noticeDto);
+        System.out.println(noticeDto);
+
+        return "admin/notice_detail";
     }
 
     @RequestMapping(value = "adminEvent.do", method = RequestMethod.GET)
@@ -58,23 +75,13 @@ public class AdminController {
     }
 
     //페이징처리한 글목록
-    @RequestMapping(value = "/listCri", method = RequestMethod.GET)
-    public void listCriGET(AdminCriteria cri, Model model) throws Exception {
-        model.addAttribute("boardList", adminService.listCri(cri));
-    }
-
-    //글목록보기(PageMaker객체 사용)
-    // http://localhost:8088/board/listPage
-    @RequestMapping(value = "/listPage", method = RequestMethod.GET)
-    public void listPageGET(AdminCriteria cri, Model model) throws Exception {
+    @RequestMapping(value = "testestsetste.do", method = RequestMethod.GET)
+    public String listCriGET(AdminCriteria cri, Model model) throws Exception {
         model.addAttribute("boardList", adminService.listCri(cri));
 
-        PageMaker pm = new PageMaker();
-        pm.setCri(cri);
-        pm.setTotalCount(adminService.pageCount()); //DB의 전체ROW수 입력
-
-        // 뷰페이지로 전달
-        model.addAttribute("pm", pm);
+        return "admin/pagingtest";
     }
+
+
 
 }
