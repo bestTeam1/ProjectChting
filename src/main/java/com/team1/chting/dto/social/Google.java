9d8fbd759241100e5c3f26c017c175495266ddd0
Sooyeon Kim <73254251+hi-sooyeon@users.noreply.github.com>
2021-06-24 07:43:09 +0900

    in progress modify sign-up logic / 30percent

---
+++ src/main/java/com/team1/chting/dto/social/Google.java
@@ -0,0 +1,61 @@
+package com.team1.chting.dto.social;
+
+import lombok.Data;
+import lombok.ToString;
+
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+@Data
+public class Google extends SocialData {
+
+    private String userid;
+    private String nickname;
+    private String email;
+    private String profile_img;
+    private String logintype;
+
+    public Google(Map<String, Object> userData, String registrationId) {
+        Set<String> userDataSet = userData.keySet();
+        Iterator<String> Iter_userDataSet = userDataSet.iterator();
+        while (Iter_userDataSet.hasNext()) {
+            String next = Iter_userDataSet.next();
+            if(next.equals("sub")) {
+                this.userid = userData.get(next).toString();
+            }else if(next.equals("name")) {
+                this.nickname = userData.get(next).toString();
+            }else if(next.equals("email")) {
+                this.email = userData.get(next).toString();
+            }else if(next.equals("picture")) {
+                this.profile_img = userData.get(next).toString();
+            }
+        }
+        this.logintype = registrationId.substring(0, registrationId.lastIndexOf("-"));
+    }
+
+    @Override
+    public String getUserid() {
+        return this.userid;
+    }
+
+    @Override
+    public String getNickname() {
+        return this.nickname;
+    }
+
+    @Override
+    public String getEmail() {
+        return this.email;
+    }
+
+    @Override
+    public String getProfile_img() {
+        return this.profile_img;
+    }
+
+    @Override
+    public String getLoginType() {
+        return this.logintype;
+    }
+}
