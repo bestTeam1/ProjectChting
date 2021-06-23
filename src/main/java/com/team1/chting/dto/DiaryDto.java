21d106a5aeee42e3093b77ab28510ce3fee81b38
123 <123>
2021-06-24 05:48:34 +0900

    fullcalendar 작업중

---
+++ src/main/java/com/team1/chting/dto/DiaryDto.java
@@ -0,0 +1,18 @@
+package com.team1.chting.dto;
+
+import lombok.Data;
+
+import java.sql.Date;
+
+@Data
+public class DiaryDto {
+	private String group_no;
+	private String type;
+	private Date startdate;
+	private String location;
+	private String content;
+	private String subject;
+	private String calDate;
+	private double xcoord;
+	private double ycoord;
+}
