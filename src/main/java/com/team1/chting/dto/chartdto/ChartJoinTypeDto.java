c50ee73e01b061a1d2958dcb918d55b2eef57cab
Zagg2732 <67727906+Zagg2732@users.noreply.github.com>
2021-06-25 19:23:24 +0900

    관리자메인 디자인개선중

---
+++ src/main/java/com/team1/chting/dto/chartdto/ChartJoinTypeDto.java
@@ -0,0 +1,11 @@
+package com.team1.chting.dto.chartdto;
+
+import lombok.Data;
+
+//차트데이터 가입유형 Dto
+@Data
+public class ChartJoinTypeDto {
+	private int kakao;
+	private int google;
+	private int github;
+}
