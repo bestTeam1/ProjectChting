797ea9a8484ac278432b8bdd5085fae91c110da3
123 <123>
2021-06-28 07:23:52 +0900

    메인이벤트 및 푸터

--- src/main/java/com/team1/chting/dao/HomeDao.java
+++ src/main/java/com/team1/chting/dao/HomeDao.java
@@ -1,10 +1,14 @@
 package com.team1.chting.dao;
 
+import com.team1.chting.dto.EventDto;
 import com.team1.chting.dto.GroupDto;
 
 import java.util.List;
 
 public interface HomeDao {
+
+    //메인 이벤트리스트
+    public List<EventDto> getEventList();
 
     //NEW 추천 모임
     public List<GroupDto> getNewGroupList();
